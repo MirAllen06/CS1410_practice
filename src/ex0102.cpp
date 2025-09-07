7d7703e1812e2f740cf96b72ddb0e9fadff7fc77
MirAllen06 <mirallen061996@gmail.com>
2025-09-07 01:13:40 +0000

    ex0101 and ex0102 written

---
+++ src/ex0102.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+    double c, f;
+    cout << "Temperature in Celcius: " << endl;
+    cin>> c;
+
+    f = 9.0 * c / 5.0 + 32;
+
+    cout << "Temperature in Fahrenheit: " <<  f << endl;
+
+    return 0;
+}
